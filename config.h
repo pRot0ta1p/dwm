8952760f668d84aa0871356feaefa139e616de63
git <git@git.com>
2021-02-19 13:37:33 +0800

    Add bar indicator feature. Modified font setup.

--- config.h
+++ config.h
@@ -8,10 +8,11 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 26;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Siji:pixelsize=16",
-                                        "WenQuanYi Micro Hei Mono:pixelsize=16:antialias=true:autohint=true" };
+static const char *fonts[]          = { "xos4 Terminus:size=14",
+                                        "Siji:pixelsize=16",
+                                        "WenQuanYi WenQuanYi Bitmap Song:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char norm_fg[] = "#e2e8ef";
 static const char norm_bg[] = "#0E2B41";
