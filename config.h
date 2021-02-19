1ba806b799cc65657e68bb78089aaf89e932ac0c
git <git@git.com>
2021-02-19 12:52:08 +0800

    Add custom bar height feature.

--- config.h
+++ config.h
@@ -9,6 +9,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
+static const int user_bh            = 26;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Siji:pixelsize=16",
                                         "WenQuanYi Micro Hei Mono:pixelsize=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=14";
