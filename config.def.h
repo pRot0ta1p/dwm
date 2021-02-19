2065ce6113c0d47910fa33b4cd8746dade864e94
git <git@git.com>
2021-02-19 19:48:31 +0800

    Add swallow feature.

--- config.def.h
+++ config.def.h
@@ -7,14 +7,15 @@ static const unsigned int gappih    = 20;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -52,9 +53,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
--- config.h
+++ config.h
@@ -1,41 +1,43 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 26;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "xos4 Terminus:size=14",
-                                        "Siji:pixelsize=16",
-                                        "WenQuanYi WenQuanYi Bitmap Song:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
-static const char norm_fg[] = "#e2e8ef";
-static const char norm_bg[] = "#0E2B41";
-static const char norm_border[] = "#9ea2a7";
-static const char sel_fg[] = "#e2e8ef";
-static const char sel_bg[] = "#78A7D3";
-static const char sel_border[] = "#e2e8ef";
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#282828";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#6F95C3";
-static const char col1[]            = "#ffffff";
-static const char col2[]            = "#ffffff";
-static const char col3[]            = "#ffffff";
-static const char col4[]            = "#ffffff";
-static const char col5[]            = "#ffffff";
-static const char col6[]            = "#ffffff";
+static const unsigned int borderpx           = 3;        /* border pixel of windows */
+static const unsigned int snap               = 32;       /* snap pixel */
+static const unsigned int gappih             = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv             = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh             = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov             = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int systraypinning     = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing     = 2;   /* systray spacing */
+static const int topbar                      = 1;        /* 0 means bottom bar */
+static const int showbar                     = 1;        /* 0 means no bar */
+static const int user_bh                     = 26;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static       int smartgaps                   = 1;        /* 1 means no outer gap when there is only one window */
+static const int showsystray                 = 1;     /* 0 means no systray */
+static const int swallowfloating             = 0;        /* 1 means swallow floating windows by default */
+static const int systraypinningfailfirst     = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const char *fonts[]                   = { "xos4 Terminus:size=14",
+                                                 "Siji:pixelsize=16",
+                                                 "WenQuanYi WenQuanYi Bitmap Song:size=14",
+                                                 "monospace:size=14"};
+static const char dmenufont[]                = "monospace:size=14";
+static const char norm_fg[]                  = "#e2e8ef";
+static const char norm_bg[]                  = "#0E2B41";
+static const char norm_border[]              = "#9ea2a7";
+static const char sel_fg[]                   = "#e2e8ef";
+static const char sel_bg[]                   = "#78A7D3";
+static const char sel_border[]               = "#e2e8ef";
+static const char col_gray1[]                = "#282828";
+static const char col_gray2[]                = "#282828";
+static const char col_gray3[]                = "#bbbbbb";
+static const char col_gray4[]                = "#eeeeee";
+static const char col_cyan[]                 = "#6F95C3";
+static const char col1[]                     = "#ffffff";
+static const char col2[]                     = "#ffffff";
+static const char col3[]                     = "#ffffff";
+static const char col4[]                     = "#ffffff";
+static const char col5[]                     = "#ffffff";
+static const char col6[]                     = "#ffffff";
 
 enum { SchemeNorm, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
        SchemeCol5, SchemeCol6, SchemeSel }; /* color schemes */
@@ -60,9 +62,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           1 << 8,    1,          0,           0,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
