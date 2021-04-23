ecd76efa5b7bf3dc739632cd73208691b31d8773
git <git@git.com>
2021-04-23 21:59:12 +0800

    gap and border adjustments.

--- config.h
+++ config.h
@@ -4,21 +4,21 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static const unsigned int borderpx           = 4;        /* border pixel of windows */
+static const unsigned int borderpx           = 5;        /* border pixel of windows */
 static const unsigned int snap               = 32;       /* snap pixel */
-static const unsigned int gappih             = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv             = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh             = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov             = 10;       /* vert outer gap between windows and screen edge */
-static const unsigned int systraypinning     = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing     = 2;   /* systray spacing */
+static const unsigned int gappih             = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv             = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh             = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov             = 5;       /* vert outer gap between windows and screen edge */
+static const unsigned int systraypinning     = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing     = 4;        /* systray spacing */
 static const int topbar                      = 1;        /* 0 means bottom bar */
 static const int showbar                     = 1;        /* 0 means no bar */
-static const int user_bh                     = 26;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh                     = 26;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static       int smartgaps                   = 1;        /* 1 means no outer gap when there is only one window */
-static const int showsystray                 = 1;     /* 0 means no systray */
+static const int showsystray                 = 1;        /* 0 means no systray */
 static const int swallowfloating             = 0;        /* 1 means swallow floating windows by default */
-static const int systraypinningfailfirst     = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int systraypinningfailfirst     = 1;        /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const char *fonts[]                   = { "xos4 Terminus:size=14",
                                                  "Siji:pixelsize=16",
                                                  "WenQuanYi WenQuanYi Bitmap Song:size=14",
@@ -56,7 +56,7 @@ enum { SchemeNorm, SchemeSel, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
 static const char *colors[][3]      = {
   /*               fg             bg               border */         /* dwmblocks colors code*/
     [SchemeNorm]  = { color7,       color0,        color0 },         /*  */
-    [SchemeSel]   = { color0,       color2,        colormagenta },   /*  */
+    [SchemeSel]   = { color0,       color2,        color6 },         /*  */
 	[SchemeCol1]  = { color7,       colorgreen,    color8 },         /*  */
 	[SchemeCol2]  = { color7,       colormagenta,  color8 },         /*  */
 	[SchemeCol3]  = { color7,       colorred,      color8 },         /*  */
